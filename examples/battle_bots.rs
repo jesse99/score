75352edb4be388e2ab8a4ad8cf0320ce668c45ed
Jesse Jones <jesse9jones@gmail.com>
2017-05-28 21:56:08 -0700

    Randomize bot positions

--- examples/battle_bots.rs
+++ examples/battle_bots.rs
@@ -1,15 +1,27 @@
 //! This example simulates a collection of battle bots with different behaviors, e.g.
 //! some of the bots flee from other bots and some are aggressive and attempt to attack
 //! other bots.
+extern crate rand;
 extern crate rsimbase;
 
+use rand::Rng;
 use rsimbase::*;
 use std::sync::mpsc;
 use std::thread;
 
 const NUM_BOTS: i32 = 4;	// TODO: make this a command line option
+const WIDTH: f64 = 100.0;	// TODO: make this a command line option
+const HEIGHT: f64 = 100.0;	// TODO: make this a command line option
 
 type ComponentThread = fn (ComponentID, mpsc::Receiver<DispatchedEvent>, mpsc::Sender<Effector>) -> ();
+
+fn randomize_location(top: ComponentID, effector: &mut Effector)
+{
+	let mut rng = rand::thread_rng();	// TODO: make sure these are using the same seed
+	let payload = (rng.gen::<f64>()*WIDTH, rng.gen::<f64>()*HEIGHT);
+	let event = Event::new_with_payload("set-location", payload);
+	effector.schedule_immediately(event, top);
+}
 
 fn cowardly_thread(id: ComponentID, rx_event: mpsc::Receiver<DispatchedEvent>,
 	tx_reply: mpsc::Sender<Effector>)
@@ -21,9 +33,7 @@ fn cowardly_thread(id: ComponentID, rx_event: mpsc::Receiver<DispatchedEvent>,
 			if ename == "init 0" {
 				effector.log(LogLevel::Info, "initializing");
 				let top = dispatched.components.find_top_id(id);
-				let payload: (f64, f64) = (1.0, 2.0);
-				let event = Event::new_with_payload("set-location", payload);
-				effector.schedule_immediately(event, top);
+				randomize_location(top, &mut effector);
 			} else {
 				let cname = &(*dispatched.components).get(id).name;
 				panic!("component {} can't handle event {}", cname, ename);
