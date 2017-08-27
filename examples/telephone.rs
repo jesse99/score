32f3c3a98d8ed17e644299005d23cd142909a52a
Jesse Jones <jesse9jones@gmail.com>
2017-08-26 19:02:48 -0700

    Added copyright notices

--- examples/telephone.rs
+++ examples/telephone.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2017 Jesse Jones
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 //! This example is a fairly crude simulation of the telephone game, aka chinese whispers.
 //! Instead of garbling a message at each step we randomly replace letters with dashes.
 //! When a message is received that contains all dashes we terminate the simulation.
--- src/values.rs
+++ src/values.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2017 Jesse Jones
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 //! `IntValue` and `FloatValue` and `StringValue` are simple wrappers around an
 //! [`Effector`]. They don't do very much but they assist in creating type safe
 //! [`Component`] structs. See the [`set_value`] macro for an example.
