c38d9b384a9820773ca335da0d7119eb9cb56d6b
Jesse Jones <jesse9jones@gmail.com>
2017-08-29 15:25:21 -0700

    Moved process_events macro

--- src/event.rs
+++ src/event.rs
@@ -85,3 +85,55 @@ impl Event
 		}
 	}
 }
+
+/// Typically `Component` threads will use this to cut down on the boiler plate involved in
+/// processing dispatched `Event`s. Note that this will panic if it tries to process an
+/// event that doesn't have an associated code block.
+///
+/// # Examples
+///
+/// ```
+/// use score::*;
+/// use std::thread;
+///
+/// fn my_thread(data: ThreadData)
+/// {
+/// 	thread::spawn(move || {
+/// 		process_events!(data, event, state, effector,
+/// 			"init 0" => {
+/// 				// Use the effector to change the simulation state.
+/// 				let event = Event::new("timer");
+/// 				effector.schedule_after_secs(event, data.id, 1.0);
+/// 			},
+/// 			"timer" => {
+/// 				// Typically you'd re-schedule the timer here,
+/// 				log_info!(effector, "timer fired!");
+/// 			}
+/// 		);
+/// 	});
+/// }
+/// ```
+#[macro_export]
+macro_rules! process_events
+{
+	($data:expr, $event:ident, $state:ident, $effector:ident, $($name:pat => $code:expr),+) => ({
+		for ($event, $state) in $data.rx.iter() {
+			let mut $effector = Effector::new();
+			{
+				let ename = &$event.name;
+				match ename.as_ref() {
+					$($name => $code)+
+					
+					_ => {
+						let cname = &(*$state.components).get($data.id).name;
+						panic!("component {} can't handle event {}", cname, ename);
+					}
+				}
+			}
+			
+			drop($state);	// we need to do this before the send to ensure that our references are dropped before the Simulator processes the send
+			let _ = $data.tx.send($effector);
+		}
+	});
+}
+
